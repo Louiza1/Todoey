2a5e8d36949dc0ab76d9b2bb74f403539f202855
louizaagroti <louiza@locumsnest.co.uk>
2019-07-11 14:45:45 +0300

    Persisting data using Core Data.

---
+++ Todoey/Controllers/CategoryViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryViewCell.swift
+//  Todoey
+//
+//  Created by Louiza Agroti on 11/07/2019.
+//  Copyright © 2019 Louiza Agroti. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
